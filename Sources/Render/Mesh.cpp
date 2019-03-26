d1fb551cebc6f8384ae5eb6a3163de0d37c45ee7
dongchao <hyrchao@outlook.com>
2019-03-27 00:19:49 +0800

    mesh class

--- Sources/Render/Mesh.cpp
+++ Sources/Render/Mesh.cpp
@@ -5,4 +5,75 @@
 //  Created by Chao on 2019/3/26.
 //
 
-#include "Mesh.hpp"
+#include "Render/Mesh.h"
+
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+{
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+    
+    SetupMesh();
+}
+
+Mesh::~Mesh()
+{
+    
+}
+
+void Mesh::SetupMesh()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    // pass the pointer of vettex struct as buffer data
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+                 &indices[0], GL_STATIC_DRAW);
+    
+    // vertex position
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    // vertex normal
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+    // vertex texcoord
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+    
+    glBindVertexArray(0);
+}
+
+void Mesh::Draw(Shader* shader)
+{
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+    for(unsigned int i = 0; i < textures.size(); i++)
+    {
+        glActiveTexture(GL_TEXTURE0 + i); // active texture
+        // get texture number（N in TypeN）
+        std::string number;
+        TextureType type = textures[i].type;
+        std::string name =TextureTypeToString(type);
+        if(type == TextureType::Diffuse)
+            number = std::to_string(diffuseNr++);
+        else if(type == TextureType::Specular)
+            number = std::to_string(specularNr++);
+        // texture naming foramt in shader : Type_num
+        shader->setFloat(("material." + name + "_" + number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+    glActiveTexture(GL_TEXTURE0);
+    
+    // draw mesh
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
--- Sources/Render/Mesh.h
+++ Sources/Render/Mesh.h
@@ -5,9 +5,68 @@
 //  Created by Chao on 2019/3/26.
 //
 
-#ifndef Mesh_hpp
-#define Mesh_hpp
+#ifndef Mesh_h
+#define Mesh_h
 
-#include <stdio.h>
+#include <chaoGL.h>
+#include <Render/Shader.h
+#include <string.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 
-#endif /* Mesh_hpp */
+struct Vertex
+{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+}
+
+struct Texture
+{
+    unsigned int id;
+    TextureType type;
+}
+
+enum TextureType
+{
+    Diffuse,
+    Normal,
+    Specular,
+    MRO,
+    Cube,
+    LUT,
+}
+
+class Mesh
+{
+public:
+    // mesh data
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> textures;
+    // func
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+    ~Mesh();
+    void Draw(Shader* shader);
+private:
+    // render buffer
+    unsigned int VAO, VBO, EBO;
+    // func
+    void SetupMesh();
+    
+    inline const char* TextureTypeToString(TextureType type)
+    {
+        switch (type)
+        {
+            case Diffuse:   return "Diffuse";
+            case Normal:   return "Normal";
+            case Specular: return "Specular";
+            case MRO: return "MRO";
+            case Cube: return "Cube";
+            case LUT: return "LUT";
+            default:      return "Diffuse";
+        }
+    }
+}
+#endif /* Mesh_h */
