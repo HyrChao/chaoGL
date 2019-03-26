17fe041706d57585a03ed31da3a651d6bc4b0a8d
dongchao <hyrchao@outlook.com>
2019-03-27 00:19:26 +0800

    Mesh class

---
+++ Sources/Render/Mesh.cpp
@@ -0,0 +1,8 @@
+//
+//  Mesh.cpp
+//  chaoGL
+//
+//  Created by Chao on 2019/3/26.
+//
+
+#include "Mesh.hpp"
---
+++ Sources/Render/Mesh.h
@@ -0,0 +1,13 @@
+//
+//  Mesh.hpp
+//  chaoGL
+//
+//  Created by Chao on 2019/3/26.
+//
+
+#ifndef Mesh_hpp
+#define Mesh_hpp
+
+#include <stdio.h>
+
+#endif /* Mesh_hpp */
